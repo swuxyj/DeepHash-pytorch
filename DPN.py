eb9416c67c67b379ceec7cc7496cda1a23de4a56
swuxyj <1142732931@qq.com>
2020-09-03 11:26:32 +0800

    Deep Polarized Network

---
+++ DPN.py
@@ -0,0 +1,171 @@
+from utils.tools import *
+from network import *
+
+import os
+import torch
+import torch.optim as optim
+import time
+import numpy as np
+import random
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+# DPN(IJCAI2020)
+# paper [Deep Polarized Network for Supervised Learning of Accurate Binary Hashing Codes](https://www.ijcai.org/Proceedings/2020/115)
+# [DPN] epoch:150, bit:48, dataset:imagenet, MAP:0.675, Best MAP: 0.688
+# [DPN] epoch:70, bit:48, dataset:cifar10-1, MAP:0.778, Best MAP: 0.787
+# [DPN] epoch:10, bit:48, dataset:nuswide_21, MAP:0.818, Best MAP: 0.818
+# [DPN-T] epoch:10, bit:48, dataset:cifar10-1, MAP:0.134, Best MAP: 0.134
+
+def get_config():
+    config = {
+        "m": 1,
+        "p": 0.5,
+        "optimizer": {"type": optim.RMSprop, "optim_params": {"lr": 1e-5, "weight_decay": 1e-5}},
+        "info": "[DPN]",
+        # "info": "[DPN-A]",
+        # "info": "[DPN-T]",
+        # "info": "[DPN-A-T]",
+        "resize_size": 256,
+        "crop_size": 224,
+        "batch_size": 32,
+        "net": AlexNet,
+        # "net": ResNet,
+        # "dataset": "cifar10-1",
+        # "dataset": "imagenet",
+        # "dataset": "coco",
+        "dataset": "nuswide_21",
+        "epoch": 150,
+        "test_map": 10,
+        # "device":torch.device("cpu"),
+        "device": torch.device("cuda:1"),
+        "bit_list": [48],
+    }
+    config = config_dataset(config)
+    return config
+
+
+class DPNLoss(torch.nn.Module):
+    def __init__(self, config, bit):
+        super(DPNLoss, self).__init__()
+        self.is_single_label = config["dataset"] not in {"nuswide_21", "nuswide_21_m", "coco"}
+        self.target_vectors = self.get_target_vectors(config["n_class"], bit, config["p"]).to(config["device"])
+        self.multi_label_random_center = torch.randint(2, (bit,)).float().to(config["device"])
+        self.m = config["m"]
+        self.U = torch.zeros(config["num_train"], bit).float().to(config["device"])
+        self.Y = torch.zeros(config["num_train"], config["n_class"]).float().to(config["device"])
+
+    def forward(self, u, y, ind, config):
+        self.U[ind, :] = u.data
+        self.Y[ind, :] = y.float()
+
+        if "-T" in config["info"]:
+            # Ternary Assignment
+            u = (u.abs() > self.m).float() * u.sign()
+
+        t = self.label2center(y)
+        polarization_loss = (self.m - u * t).clamp(0).mean()
+
+        return polarization_loss
+
+    def label2center(self, y):
+        if self.is_single_label:
+            hash_center = self.target_vectors[y.argmax(axis=1)]
+        else:
+            # for multi label, use the same strategy as CSQ
+            center_sum = y @ self.target_vectors
+            random_center = self.multi_label_random_center.repeat(center_sum.shape[0], 1)
+            center_sum[center_sum == 0] = random_center[center_sum == 0]
+            hash_center = 2 * (center_sum > 0).float() - 1
+        return hash_center
+
+    # Random Assignments of Target Vectors
+    def get_target_vectors(self, n_class, bit, p=0.5):
+        target_vectors = torch.zeros(n_class, bit)
+        for k in range(20):
+            for index in range(n_class):
+                ones = torch.ones(bit)
+                sa = random.sample(list(range(bit)), int(bit * p))
+                ones[sa] = -1
+                target_vectors[index] = ones
+        return target_vectors
+
+    # Adaptive Updating
+    def update_target_vectors(self):
+        self.U = (self.U.abs() > self.m).float() * self.U.sign()
+        self.target_vectors = (self.Y.t() @ self.U).sign()
+
+
+def train_val(config, bit):
+    device = config["device"]
+    train_loader, test_loader, dataset_loader, num_train, num_test, num_dataset = get_data(config)
+    config["num_train"] = num_train
+    net = config["net"](bit).to(device)
+
+    optimizer = config["optimizer"]["type"](net.parameters(), **(config["optimizer"]["optim_params"]))
+
+    criterion = DPNLoss(config, bit)
+
+    Best_mAP = 0
+
+    for epoch in range(config["epoch"]):
+
+        current_time = time.strftime('%H:%M:%S', time.localtime(time.time()))
+
+        print("%s[%2d/%2d][%s] bit:%d, dataset:%s, training...." % (
+            config["info"], epoch + 1, config["epoch"], current_time, bit, config["dataset"]), end="")
+
+        net.train()
+
+        train_loss = 0
+        for image, label, ind in train_loader:
+            image = image.to(device)
+            label = label.to(device)
+
+            optimizer.zero_grad()
+            u = net(image)
+
+            loss = criterion(u, label.float(), ind, config)
+            train_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+        if "-A" in config["info"]:
+            criterion.update_target_vectors()
+
+        train_loss = train_loss / len(train_loader)
+
+        print("\b\b\b\b\b\b\b loss:%.3f" % (train_loss))
+
+        if (epoch + 1) % config["test_map"] == 0:
+            # print("calculating test binary code......")
+            tst_binary, tst_label = compute_result(test_loader, net, device=device)
+
+            # print("calculating dataset binary code.......")\
+            trn_binary, trn_label = compute_result(dataset_loader, net, device=device)
+
+            # print("calculating map.......")
+            mAP = CalcTopMap(trn_binary.numpy(), tst_binary.numpy(), trn_label.numpy(), tst_label.numpy(),
+                             config["topK"])
+
+            if mAP > Best_mAP:
+                Best_mAP = mAP
+
+                if "save_path" in config:
+                    if not os.path.exists(config["save_path"]):
+                        os.makedirs(config["save_path"])
+                    print("save in ", config["save_path"])
+                    np.save(os.path.join(config["save_path"], config["dataset"] + str(mAP) + "-" + "trn_binary.npy"),
+                            trn_binary.numpy())
+                    torch.save(net.state_dict(),
+                               os.path.join(config["save_path"], config["dataset"] + "-" + str(mAP) + "-model.pt"))
+            print("%s epoch:%d, bit:%d, dataset:%s, MAP:%.3f, Best MAP: %.3f" % (
+                config["info"], epoch + 1, bit, config["dataset"], mAP, Best_mAP))
+            print(config)
+
+
+if __name__ == "__main__":
+    config = get_config()
+    print(config)
+    for bit in config["bit_list"]:
+        train_val(config, bit)
