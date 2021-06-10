90b72e52142badd8d7f0358ec2d17b4215210e64
swuxyj <1142732931@qq.com>
2021-06-10 11:59:34 +0800

    Delete QSMIH.py

--- https:/github.com/passalis/QSMIH.py
+++
@@ -1,6 +0,0 @@
-
-# QSMIH(Signal Processing: Image Communication)
-# paper [Deep supervised hashing using quadratic spherical mutual information for efficient image retrieval](https://www.sciencedirect.com/science/article/pii/S0923596521000072)
-# code [qsmi  pytorch](https://github.com/passalis/qsmi)
-
-# todo
